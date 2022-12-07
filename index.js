24b77961c72174ce42441a2877ada1ea5c1e0e47
Khumbongmayum Tonny <tonnykh@icloud.com>
2022-12-07 19:58:28 +0530

    Add input search box

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-let cityName = "London";
+// let cityName = "London";
 
-
-// return weather data for that city
 async function getData(city) {
     try {
-        const response = await fetch(`https://api.openweathermaporg/data/2.5/weather?q=${city}appid=42228af3335be34c97a5ecec9960067b`, { mode:'cors' });
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=42228af3335be34c97a5ecec9960067b`, { mode: 'cors' });
 
         const locationData = await response.json();
         return locationData;
     } catch (error) {
     }
-};
-
-// get data & return only the required data
-async function getWeatherData(locationName) {
-    let locationData = await getData(locationName);
-    console.log(locationData.weather[0], "nknkn");
 }
 
-getWeatherData(cityName);
+async function getWeatherData(locationName) {
+    let locationData = await getData(locationName);
+    // console.log(locationName);
+    // console.log(locationData.weather[0]);
+    console.log(locationData.weather[0].description);
+}
+    
+// getWeatherData(cityName);
+
+
+// DOM
+const searchInput = document.querySelector("input");
+const searchForm = document.querySelector("form")
+
+searchForm.addEventListener("submit", (event) => {
+    console.log(searchInput.value);
+    event.preventDefault();
+    getWeatherData(searchInput.value);
+})
