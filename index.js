7858239634568287e1b62a638489aac0685a5419
Khumbongmayum Tonny <tonnykh@icloud.com>
2022-12-07 21:32:01 +0530

    Add search result visibility

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
-// let cityName = "London";
-
 async function getData(city) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=42228af3335be34c97a5ecec9960067b`, { mode: 'cors' });
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=42228af3335be34c97a5ecec9960067b`, { mode: 'cors' });
 
         const locationData = await response.json();
         return locationData;
+
     } catch (error) {
     }
 }
 
-async function getWeatherData(locationName) {
-    let locationData = await getData(locationName);
-    // console.log(locationName);
-    // console.log(locationData.weather[0]);
-    console.log(locationData.weather[0].description);
+
+function processData(cityData) {
+    const {
+      name: cityName,
+      main: { temp: temperature, feels_like: feelsLike, humidity },
+      wind: { speed: windSpeed },
+    } = cityData;
+
+    return { cityName, temperature, feelsLike, humidity, windSpeed };
 }
     
-// getWeatherData(cityName);
 
 
 // DOM
+
+// Submit listen
 const searchInput = document.querySelector("input");
 const searchForm = document.querySelector("form")
 
 searchForm.addEventListener("submit", (event) => {
-    console.log(searchInput.value);
     event.preventDefault();
-    getWeatherData(searchInput.value);
+    displayResult(searchInput.value);
 })
+
+// search result display
+const cityName = document.querySelector("#cityName");
+const temperature = document.querySelector("#temperature");
+const feelsLike = document.querySelector("#feelsLike");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
+
+async function displayResult(city) {
+
+    let data = await getData(city);
+
+    let newData = processData(data);
+
+    cityName.textContent = newData.cityName;
+    temperature.textContent = `${newData.temperature} °C`;
+    feelsLike.textContent = `Feels like: ${newData.feelsLike} °C`;
+    humidity.textContent = `Humidity: ${newData.humidity} %`;
+    wind.textContent = `Wind: ${newData.windSpeed} km/h`;
+}
+
+displayResult("delhi");
