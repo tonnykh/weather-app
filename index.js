b62ca22fec5fcb004fc33eef06040c534b5e161a
Khumbongmayum Tonny <tonnykh@icloud.com>
2022-12-08 10:36:24 +0530

    Add gitignore, eslint, preetier

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
-async function getData(city) {
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=42228af3335be34c97a5ecec9960067b`, { mode: 'cors' });
-
-        const locationData = await response.json();
-        return locationData;
-
-    } catch (error) {
-    }
-}
-
-
-function processData(cityData) {
-    const {
-      name: cityName,
-      main: { temp: temperature, feels_like: feelsLike, humidity },
-      wind: { speed: windSpeed },
-    } = cityData;
-
-    return { cityName, temperature, feelsLike, humidity, windSpeed };
-}
-    
-
-
 // DOM
+const cityName = document.querySelector("#cityName");
+const temperature = document.querySelector('#temperature');
+const feelsLike = document.querySelector('#feelsLike');
+const humidity = document.querySelector('#humidity');
+const wind = document.querySelector('#wind');
 
-// Submit listen
-const searchInput = document.querySelector("input");
-const searchForm = document.querySelector("form")
+const searchInput = document.querySelector('input');
+const searchForm = document.querySelector('form');
 
-searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    displayResult(searchInput.value);
-})
+
+// Input Submit listen
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (searchInput.value === '') return;
+
+  displayResult(searchInput.value);
+});
+
 
 // search result display
-const cityName = document.querySelector("#cityName");
-const temperature = document.querySelector("#temperature");
-const feelsLike = document.querySelector("#feelsLike");
-const humidity = document.querySelector("#humidity");
-const wind = document.querySelector("#wind");
+async function displayResult(userInputCityName) {
+  const data = await getData(userInputCityName);
+  if (!data) return;
 
-async function displayResult(city) {
-
-    let data = await getData(city);
-
-    let newData = processData(data);
-
-    cityName.textContent = newData.cityName;
-    temperature.textContent = `${newData.temperature} °C`;
-    feelsLike.textContent = `Feels like: ${newData.feelsLike} °C`;
-    humidity.textContent = `Humidity: ${newData.humidity} %`;
-    wind.textContent = `Wind: ${newData.windSpeed} km/h`;
+  cityName.textContent = data.cityName;
+  temperature.textContent = `${data.temperature} °C`;
+  feelsLike.textContent = `Feels like: ${data.feelsLike} °C`;
+  humidity.textContent = `Humidity: ${data.humidity} %`;
+  wind.textContent = `Wind: ${data.windSpeed} km/h`;
 }
 
-displayResult("delhi");
+
+// API fetch data
+async function getData(userInputCityName) {
+  const endpoint = (`https://api.openweathermap.org/data/2.5/weather?q=${userInputCityName}&units=metric&appid=42228af3335be34c97a5ecec9960067b`);
+
+  try {
+    const response = await fetch(endpoint, { mode: 'cors' });
+    if (!response.ok) throw new Error(`City "${userInputCityName}" not found`);
+    const locationData = processData(await response.json());
+    return locationData;
+  } catch (error) {
+    alert(error);
+    return null;
+  }
+}
+
+
+// data process
+function processData(cityWeatherData) {
+  const {
+    name: cityName,
+    main: { temp: temperature, feels_like: feelsLike, humidity },
+    wind: { speed: windSpeed },
+  } = cityWeatherData;
+
+  return {
+    cityName, temperature, feelsLike, humidity, windSpeed,
+  };
+}
+
+
+displayResult('0-0-');
